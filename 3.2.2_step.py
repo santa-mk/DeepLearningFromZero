4a851fd22b80f33a2d2268d9d3fe4279996355f8
santa-mk <maguielaw.elir.107@gmail.com>
2019-04-18 22:37:36 +0900

    [add] implement np array step function

--- 3.2.2_step.py
+++ 3.2.2_step.py
@@ -1,8 +1,20 @@
-def step_function(x):
+import numpy as np
+
+def step_function_num(x):
     if x > 0:
         return 1
     else:
         return 0
 
-print("step(0) : " + str(step_function(0)))
-print("step(1) : " + str(step_function(1)))
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+print("step(0) : " + str(step_function_num(0)))
+print("step(1) : " + str(step_function_num(1)))
+
+array = np.array([-1.0, 1.0, 2.0])
+print("np array")
+print(array)
+print("np array stepped")
+print(step_function(array))
