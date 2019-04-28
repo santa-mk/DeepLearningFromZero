5bb27ad908df1cbcb283eb514a427c41032ebb20
santa-mk <maguielaw.elir.107@gmail.com>
2019-04-28 22:56:24 +0900

    [add] implement mnist sample

---
+++ 3.6.1_mnist.py
@@ -0,0 +1,32 @@
+import numpy as np
+from PIL import Image
+import sys, os
+sys.path.append(os.pardir + "/deep-learning-from-scratch") # setting to import parent dir files
+from dataset.mnist import load_mnist
+
+def mnist_test():
+    # wait some minutes for first call...
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+    # output each data formation
+    print(x_train.shape) # (60000, 784)
+    print(t_train.shape) # (60000, )
+    print(x_test.shape) # (10000, 784)
+    print(t_test.shape) # (10000)
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+def mnist_img():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+    img = x_train[0]
+    label = t_train[0]
+    print(label) # 5
+    
+    print(img.shape)  # (784,)
+    img = img.reshape(28, 28) # transfrom original image size
+    print(img.shape) # (28, 28)
+    
+    img_show(img)
+
+mnist_img()
