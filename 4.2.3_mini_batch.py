d1024fee4e9324cfcb8267e740bba2970beeaddc
santa-mk <maguielaw.elir.107@gmail.com>
2019-05-13 00:22:31 +0900

    [add] 4.2.4 cross entropy error wtih mini batch

--- 4.2.3_mini_batch.py
+++ 4.2.3_mini_batch.py
@@ -3,6 +3,14 @@ import sys, os
 sys.path.append(os.pardir + "/deep-learning-from-scratch") # setting to import parent dir files
 from dataset.mnist import load_mnist
 
+def cross_entropy_erro(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize = True, one_hot_label = True)
 print(x_train.shape)
 print(t_train.shape)
